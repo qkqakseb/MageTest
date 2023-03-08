e52a860b471f9aff86c78a759e5e3bc51696a8ba
qkqakseb <parknanju6376@gmail.com>
2023-03-08 11:29:16 +0900

    [main] setup

---
+++ MgTest/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace MgTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ruler ruler = new Ruler(10);
+            ruler.Run();
+        }
+    }
+
+    public class Ruler
+    {
+        private const float ONE_INCH = 2.54f;
+        public int Centimeter { get; set; } = 0;
+        public float Inch
+        {
+            get { return Centimeter * ONE_INCH; }
+            private set { Centimeter = (int)(value / ONE_INCH); }
+        }
+        public Ruler(int cmValue) { Centimeter = cmValue; }
+
+        public void Run()
+        {
+            Console.WriteLine($"{this.Centimeter}cm 는 {this.Inch} inch 입니다.");
+        }
+    }
+}
